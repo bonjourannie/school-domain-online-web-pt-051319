a4e9c1e429963c9a157c5a20ef00aec61221c9a5
Annie Truex <artruex@gmail.com>
2019-06-01 00:09:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,30 @@
 # code here!
 class School
- 	attr_accessor :roster, :name, :grade 
-	attr_reader :school 
-	
-	def initialize(school)
-    @school = school
-  end
-  
-  def roster
-  	roster = {}
-  end 
-  
-  def add_student(name,grade)
-  	roster[grade] = []
-  	roster[grade] << name
-  		
+ attr_accessor :name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
   end
 
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_hash = {}
+    @roster.sort.each do |grade, name_array|
+      sorted_hash[grade] = (name_array.sort)
+    end
+    sorted_hash
+  end
+end
 end
