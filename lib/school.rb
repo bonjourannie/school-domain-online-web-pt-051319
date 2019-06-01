43ad0d79db7258d5abc8090ec068de789640771a
Annie Truex <artruex@gmail.com>
2019-06-01 00:04:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
     @roster
   end
   
-  def add_student (student_name, grade)
+  def add_student (student_name, stuent_grade)
     @student_name = student_name
     @student_grade = student_grade
   end
