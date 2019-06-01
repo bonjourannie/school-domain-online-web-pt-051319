271db2a0464bb2d5361bab0283cd67419fa862a4
Annie Truex <artruex@gmail.com>
2019-06-01 00:07:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,20 @@
 # code here!
 class School
-  
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
+ 	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
   end
   
   def roster
-    @roster
-  end
+  	roster = {}
+  end 
   
-  def add_student (student_name, student_grade)
-    @student_name = student_name
-    @student_grade = student_grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-    end
-    @roster[grade] << name
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
   end
 
-  def grade(number)
-    @roster[number]
-  end
-
-  end
-  
 end
